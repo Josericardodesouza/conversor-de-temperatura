b18be1559603212e02188a2b44107be214f5d9e5
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2024-08-13 00:55:56 -0300

    update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ function teste() {
     // var temp = document.getElementById("select").value
 
     var tempInput = parseFloat(document.getElementById("input-ent").value) // valor digitado no input
+    var pGraus = document.getElementById("p-graus")
     var inputRadio = document.querySelector('input[name = "input-radio"]:checked').value // pega o valor do input-radio marcado
 
     var tempSelect = document.getElementById("select").value // pega a opção selecionada no select
@@ -13,18 +14,34 @@ function teste() {
 
     var sectionRes = document.getElementById("section-saida")
 
+    if (tempInput != "") {
+        
+    sectionRes.style.display = 'flex'
+
+    } else {
+        sectionRes.style.display = 'none'
+    }
+
+
+
+
     switch (tempSelect) {
         case ('c'): // VALOR do input para CELSIUS
             if (inputRadio === "c") { // convertendo DE celsius para celsius
                 p.innerHTML = tempInput.toFixed(1)
+                pGraus.innerHTML = " ºC"
+                
+              
 
             } else if (inputRadio === "f") { // convert.  DE fahrenheith para celsius
                 var res = (tempInput - 32) / 1.8
-                p.innerHTML = res.toFixed(1)
+                p.innerText = res.toFixed(1) 
+                pGraus.innerHTML = " ºF"
 
             } else if (inputRadio === "k") { // convert. DE kelvin para celsius
                 var res = (tempInput - 273.15)
-                p.innerHTML = res.toFixed(1)
+                p.innerText = res.toFixed(1) 
+                pGraus.innerHTML = " K"
             }
             break;
 
@@ -33,10 +50,10 @@ function teste() {
         case ('f'): // VALOR do input para FAHREN.
             if (inputRadio === "c") { // convert. DE celsius para FAHREN.
                 var res = (tempInput * 1.8) + 32
-                p.innerHTML = res.toFixed(1)
+                p.innerHTML = res.toFixed(1) 
             
             } else if (inputRadio === "f") { // convert. DE FAHREN. para FAHREN.
-                p.innerHTML = tempInput.toFixed(1)
+                p.innerHTML = tempInput.toFixed(1) 
 
 
             } else if (inputRadio === "k") { // convert. DE Kelvin para FAHREN.
@@ -51,7 +68,7 @@ function teste() {
             case ('k'):
                 if (inputRadio === "c") { // convert. DE celsius para kelvin.
                     var res = tempInput + 273.15
-                    p.innerHTML = res.toFixed(1)
+                    p.innerHTML = res.toFixed(1) 
                
                 } else if (inputRadio === "f") { // convert. DE FAHREN. para kelvin.
                     var res = (tempInput - 32) / 9 * 5
@@ -81,9 +98,18 @@ function teste() {
 function texto() { // função que muda a imagem do resultado a depender do resultado da conversão
     
     var p = document.getElementById("saida").innerText
+    var numTemp = document.getElementById("saida")
+    var pGraus = document.getElementById("p-graus")
     var tempSelect = document.getElementById("select").value
     var frase = document.getElementById("p-frase")
     var img = document.getElementById("imgSaida")
+
+    var corMf = "#12106f"
+    var corFrio = "#9B9ECE"
+    var corAmeno = "#90E39A"
+    var corQuente = "#FB8B24"
+    var corMq = "#E36414"
+    
 
 
     console.log("teste")
@@ -91,29 +117,41 @@ function texto() { // função que muda a imagem do resultado a depender do resu
     switch (tempSelect) {
         case ('c'): // caso a conversão foi para celsius
             if (p < 1) {
+                
                 frase.innerHTML = "Muito frio"
-
-                img.setAttribute('src' ,'images/pexels-pixabay-358235.jpg');
+                img.setAttribute('src' ,'images/pexels-pixabay-358235.jpg')
+                numTemp.style.color = corMf
+                pGraus.style.color = corMf
+              
+            
+                
 
 
             } else if (p > 1 && p < 10) {
                 frase.innerHTML = "Frio"
-                frase.setAttribute('style', 'color: green')
-
+                numTemp.style.color = corFrio
+                pGraus.style.color = corFrio
                 img.setAttribute('src', 'images/pexels-emmanuel-codden-1502600-17169274.jpg')
              
 
             } else if (p > 10 && p < 20) {
                 frase.innerHTML = "Ameno"
+                numTemp.style.color = corAmeno
+                pGraus.style.color = corAmeno
                 img.setAttribute('src', 'images/pexels-sherissa-r-704096.jpg')
 
 
             } else if (p > 20 && p < 30) {
                 frase.innerHTML = "Quente"
+                numTemp.style.color = corQuente
+                pGraus.style.color = corQuente
                 img.setAttribute('src', 'images/pexels-viniciusvieirafotografia-2559997.jpg')
 
             } else if (p > 30) {
                 frase.innerHTML = "Muito quente"
+                numTemp.style.color = corMq
+                pGraus.style.color = corMq
+            
                 img.setAttribute('src', 'images/pexels-mark-neal-201020-3061495.jpg')
                 
             }
@@ -123,24 +161,36 @@ function texto() { // função que muda a imagem do resultado a depender do resu
         case ('f'):
             if (p < 32) {
                 frase.innerHTML = "Muito frio"
-                img.setAttribute('src' ,'images/pexels-pixabay-358235.jpg');
+                numTemp.style.color = corMf
+                pGraus.style.color = corMf
+                img.setAttribute('src' ,'images/pexels-pixabay-358235.jpg')
              
 
             } else if (p > 32 && p < 60) {
                 frase.innerHTML = "Frio"
-                frase.setAttribute('style', 'color: green')
+                numTemp.style.color = corFrio
+                pGraus.style.color = corFrio
+                img.setAttribute('src', 'images/pexels-emmanuel-codden-1502600-17169274.jpg')
+
+                
 
             } else if (p > 60 && p < 77 ) {
                 frase.innerHTML = "Ameno"
+                numTemp.style.color = corAmeno
+                pGraus.style.color = corAmeno
                 img.setAttribute('src', 'images/pexels-sherissa-r-704096.jpg')
 
             } else if (p > 77 && p < 95) {
                 frase.innerHTML = "Quente"
+                numTemp.style.color = corQuente
+                pGraus.style.color = corQuente
                 img.setAttribute('src', 'images/pexels-viniciusvieirafotografia-2559997.jpg')
             
 
             } else if (p > 95) {
                 frase.innerHTML = "Muito quente"
+                numTemp.style.color = corMq
+                pGraus.style.color = corMq
                 img.setAttribute('src', 'images/pexels-mark-neal-201020-3061495.jpg')
             }
 
@@ -149,22 +199,32 @@ function texto() { // função que muda a imagem do resultado a depender do resu
         case ('k'):
             if (p < 273) {
                 frase.innerHTML = "Muito frio"
-                img.setAttribute('src' ,'images/pexels-pixabay-358235.jpg');
+                numTemp.style.color = corMf
+                pGraus.style.color = corMf
+                img.setAttribute('src' , 'images/pexels-pixabay-358235.jpg')
 
             } else if (p > 273 && p < 288) {
                 frase.innerHTML = "Frio"
+                numTemp.style.color = corFrio
+                pGraus.style.color = corFrio
                 frase.setAttribute('style', 'color: green')
 
             } else if (p > 288 && p < 298) {
                 frase.innerHTML = "Ameno"
+                numTemp.style.color = corAmeno
+                pGraus.style.color = corAmeno
                 img.setAttribute('src', 'images/pexels-sherissa-r-704096.jpg')
 
             } else if (p > 298 && p < 308) {
                 frase.innerHTML = "Quente"
+                numTemp.style.color = corQuente
+                pGraus.style.color = corQuente
                 img.setAttribute('src', 'images/pexels-viniciusvieirafotografia-2559997.jpg')
 
             } else if (p > 308) {
                 frase.innerHTML = "Muito quente"
+                numTemp.style.color = corMq
+                pGraus.style.color = corMq
                 img.setAttribute('src', 'images/pexels-mark-neal-201020-3061495.jpg')
 
             }
@@ -184,9 +244,12 @@ function reiniciar() {
     var top = document.getElementById("tituloh2")
     var p = document.getElementById("saida")
     var input = document.getElementById("input-ent")
+    var section = document.getElementById("section-saida")
 
      p.innerHTML = ""
      input.value = ""
+     section.style.display = "none"
+
 
      console.log("chamou")
 
